437bb04aa0d4494760f1f52ecd34a7f66a7cfbea
Júlio Dias <julio.dias@trizy.com.br>
2025-11-16 23:46:41 -0300

    Corrigindo o codigo

--- Program.cs
+++ Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Diagnostics.Contracts;
-using System.Runtime.InteropServices.Marshalling;
 
 namespace MeuApp;
 public class Program
 {
     public static void Main(string[] args) {
-        
+
         }
 }
